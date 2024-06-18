a977bf23d2fce470ce607b941b543169382417cc
VinitCoding <vinitcoding1001@gmail.com>
2024-06-18 15:18:29 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/display_data' element={() => {}}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section>
+        {/*  */}
+        <div>
+            <img src="" alt="" />
+
+            <div>
+                <h2></h2>
+                <p></p>
+            </div>
+        </div>
+
+        {/*  */}
+        <div>
+
+        </div>
+    </section>
+  )
+}
+
+export default Home
